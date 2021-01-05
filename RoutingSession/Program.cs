cf766a59f3956b18225241988fa9a35bea0857d1
Muhammad Zubair <Muhammad Zubair@DESKTOP-A92JIVF>
2021-01-05 15:43:32 +0200

    Login Performed On Page

--- RoutingSession/Program.cs
+++ RoutingSession/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using RoutingSession.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,42 @@ namespace RoutingSession
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Initiating...!!!");
+            var webDriver = LaunchBrowser();
+
+            //Code
+            //=========================================================
+            try
+            {
+                var connectionService = new ConnectionService(webDriver);
+                connectionService.PerformLogin("user", "tartu7777");
+
+                //Console.WriteLine(connectionService.fetchStatusPage());
+                System.Threading.Thread.Sleep(2000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex);
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
+
+            Console.WriteLine("\nDone...");
+            //=========================================================
+
+            Console.ReadKey();
+        }
+
+        static IWebDriver LaunchBrowser()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximised");
+            options.AddArgument("--disable-notifications");
+
+            var driver = new ChromeDriver(Environment.CurrentDirectory, options);
+            return driver;
         }
     }
 }
---
+++ RoutingSession/Services/ConnectionService.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutingSession.Services
+{
+    public class ConnectionService
+    {
+        private static string RouterUrl = "http://192.168.2.1";
+        private readonly IWebDriver driver;
+
+        public ConnectionService(IWebDriver _d)
+        {
+            this.driver = _d;
+            this.driver.Url = RouterUrl;
+        }
+
+        
+        public void PerformLogin(string Username,string Password)
+        {
+            driver.FindElement(By.Id("inputEmail")).Clear();
+            driver.FindElement(By.Id("inputEmail")).SendKeys(Username);
+
+            driver.FindElement(By.Id("inputPassword")).Clear();
+            driver.FindElement(By.Id("inputPassword")).SendKeys(Password);
+
+            ClickAndWaitForPageToLoad(driver, By.ClassName("btn"));
+        }
+
+        private  void ClickAndWaitForPageToLoad(IWebDriver driver, By elementLocator, int timeout = 10)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+                var elements = driver.FindElements(elementLocator);
+
+                if (elements.Count == 0)
+                {
+                    throw new NoSuchElementException(
+                        "Exception: No Elements " + elementLocator + " ClickAndWaitForPageToLoad");
+                }
+                var element = elements.FirstOrDefault(e => e.Displayed);
+                element.Click();
+                wait.Until(ExpectedConditions.StalenessOf(element));
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine(
+                    "Exception: Element with locator: '" + elementLocator + "' was not found");
+                throw;
+            }
+        }
+
+        public  string fetchStatusPage()
+        {
+            driver.Navigate().GoToUrl(RouterUrl + "/#!/wireless-status");
+            return driver.PageSource.ToString();
+        }
+    }
+}
